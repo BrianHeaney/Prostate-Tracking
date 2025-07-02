d483cb63e6ddf768f11231c98e79363bb0567b7d
BrianHeaney <83079234+BrianHeaney@users.noreply.github.com>
2025-07-02 13:44:09 -0700

    Implemented a 3x3 search window to find the closest match (lowest square error) in a new image compared to the reference ROI image

--- main.py
+++ main.py
@@ -33,10 +33,13 @@ if __name__ == "__main__":
     # Extract the human-determined ROI (prostate) from full ROI U/S frame
     roiRect = Rect(439, 226, 182, 195)
     #x, y, w, h = 439, 226, 182, 195
-    roiFrame     = cv.imread(roiFile, cv.IMREAD_COLOR)
+    roiFrame     = cv.imread  (roiFile , cv.IMREAD_COLOR)
     roiGrayFrame = cv.cvtColor(roiFrame, cv.COLOR_BGR2GRAY)
     roiImage     = roiGrayFrame[roiRect.y:roiRect.y+roiRect.h, roiRect.x:roiRect.x+roiRect.w]
     cv.imshow("Prostate in frame 1", roiImage)
+
+    newFrame     = cv.imread  (newFile , cv.IMREAD_COLOR)
+    newGrayFrame = cv.cvtColor(newFrame, cv.COLOR_BGR2GRAY)
 
     # Draw a red rect around the ROI in the full u/s frame
     annotatedFrame = roiFrame.copy()
@@ -44,81 +47,40 @@ if __name__ == "__main__":
                                 (roiRect.x + roiRect.w, roiRect.y + roiRect.h), 
                                 (0, 0, 255), 1) 
   
+    # find the ROI in the current frame by searching a 
+    searchWindow = 3   # num pixels around the ROI rect to search for a match
+    diffList = []
+    index = minIndex = 0
+    minErr = 0
+    minX = minY = 0
+    for x in range(roiRect.x - searchWindow, roiRect.x + searchWindow):
+         for y in range(roiRect.y - searchWindow, roiRect.y + searchWindow):
+            # extract the ROI rect from the new image, located at <x, y>
+            newImage = newGrayFrame[y:y+roiRect.h, x:x+roiRect.w]
+            diff = cv.subtract(roiImage, newImage)
+            err = np.sum(diff**2)
+            if index == 0 or err < minErr:
+                minIndex = index
+                minErr = err
+                minX = x
+                minY = y
 
-    # extract the transverse and sagittal scans
-    # x, y, w, h = 198, 91, 795, 453  
-    # transverseImage = roiGrayFrame[y:y+h, x:x+w]
-    #cv.imshow('Transverse', transverseImage) 
-    # annotatedFrame = cv.rectangle(annotatedFrame, (x, y), 
-    #                             (x + w, y + h), 
-    #                             (255, 0, 0), 2) 
+            mse = err/(float(roiImage.size))
+            diffList.append((index, x, y, err))
+            index += 1
 
-
-    # x, y, w, h = 198, y+h, 795, 464 
-    # sagittalImage = roiGrayFrame[y:y+h, x:x+w]
-    #cv.imshow('Sagittal', sagittalImage) 
-    # annotatedFrame = cv.rectangle(annotatedFrame, (x, y), 
-    #                             (x + w, y + h), 
-    #                             (255, 255, 0), 2) 
-
-    # find the ROI in the current frame
-
-
+    # find the best match (least error) in the search window
+    diff = diffList[minIndex]
+    newImageRoi = newGrayFrame[minY:minY+roiRect.h, minX:minX+roiRect.w]
+    newAnnotatedFrame = cv.rectangle(newFrame, (minX, minY), 
+                                (minX + roiRect.w, minY + roiRect.h), 
+                                (0, 255, 255), 1) 
 
     while True:
-        cv.imshow('Frame', annotatedFrame) # Display the full ultrasound image
+        cv.imshow('ROI Frame', annotatedFrame) # Display the full ultrasound image
+        cv.imshow('new Frame', newAnnotatedFrame) # Display the full ultrasound image
         if cv.waitKey(25) & 0xFF == ord('q'):
             cv.destroyAllWindows()
             exit()
-    # detectCircles = DetectCircles()
-            
-    # cv.namedWindow(detectCircles.processedImageWindowTitle)
-    # cv.createTrackbar("Blur" , detectCircles.processedImageWindowTitle,  1, 20, detectCircles.setBlur )
-    # cv.createTrackbar("Alpha", detectCircles.processedImageWindowTitle, 28, 50, detectCircles.setAlpha)
-    # cv.createTrackbar("Beta" , detectCircles.processedImageWindowTitle, 49, 60, detectCircles.setBeta )
-    # cv.createTrackbar("Thresh" , detectCircles.processedImageWindowTitle, 150, 200, detectCircles.setThresh)
-    # cv.createTrackbar("Maxval" , detectCircles.processedImageWindowTitle, 200, 255, detectCircles.setMaxval)
-
-    # if filename.find('.avi') > 0:
-    #     video = cv.VideoCapture(filename)
-    #     if not video.isOpened():
-    #         print("empty video, exiting...")
-    #         exit(-1)
-
-    #     while True:
-    #         ret, ultrasoundFrame = video.read() # Read a frame
-
-    #         if not ret: # If frame is not read correctly, stream has ended
-    #             print("Can't receive frame (stream end?). Restarting video...")
-    #             video.set(cv.CAP_PROP_POS_FRAMES, 0)
-    #             ret, ultrasoundFrame = video.read() # Read a frame
-            
-    #         grayImage = cv.cvtColor(ultrasoundFrame, cv.COLOR_BGR2GRAY)
-    #         # get and display the transverse scan
-    #         x, y, w, h = 198, 91, 795, 453  
-    #         transverseImage = grayImage[y:y+h, x:x+w]
-    #         cv.imshow('Transverse', transverseImage) 
-    #         # ultrasoundImage = cv.rectangle(ultrasoundImage, (x, y), 
-    #         #                             (x + w, y + h), 
-    #         #                             (255, 0, 0), 2) 
-
-    #         # get and display the sagittal scan
-    #         x, y, w, h = 198, y+h, 795, 464 
-    #         sagittalImage = grayImage[y:y+h, x:x+w]
-    #         cv.imshow('Sagittal', sagittalImage) 
-    #         # ultrasoundImage = cv.rectangle(ultrasoundImage, (x, y), 
-    #         #                             (x + w, y + h), 
-    #         #                             (255, 255, 0), 2) 
-
-    #         cv.imshow('Video', grayImage) # Display the full ultrasound image
-
-    #         # Process the sagittal image, drawing circles around any found circles
-    #         detectCircles.processImage(sagittalImage)
-
-    #         # Wait for a key press for a short duration (e.g., 25ms)
-    #         # Press 'q' to quit
-    #         if cv.waitKey(25) & 0xFF == ord('q'):
-    #             exit()
-
-    # exit(0)
+   
  
